fee8bd174dd1b8abeaa05e172fb1d4c7add9585e
Albert Yee <albertyee84@gmail.com>
2019-12-02 12:58:59 -0800

    added algorithms and solutions

--- merge_sort_project/lib/merge_sort.js
+++ merge_sort_project/lib/merge_sort.js
@@ -8,6 +8,7 @@ function merge(array1, array2) {
         }
     }
     return result.concat(array1).concat(array2);
+    //result.concat(array1, array2)
 }
 
 function mergeSort(array) {
@@ -24,3 +25,28 @@ module.exports = {
     merge,
     mergeSort
 };
+
+//https://open.appacademy.io/learn/swe-in-person/career-quest/merge-sort-notes
+
+//Time Complexity: O(n log(n))
+// n is the length of the input array
+// We must calculate how many recursive calls we make. The number of recursive calls is the number of times we must split the array to reach the base case. Since we split in half each time, the number of recursive calls is O(log(n)).
+// for example, say we had an array of length 32
+// then the length would change as 32 -> 16 -> 8 -> 4 -> 2 -> 1, we have to split 5 times before reaching the base case, log(32) = 5
+// in our algorithm, log(n) describes how many times we must halve n until the quantity reaches 1.
+// Besides the recursive calls, we must consider the while loop within the merge function, which contributes O(n) in isolation
+// We call merge in every recursive mergeSort call, so the total complexity is O(n * log(n))
+// Space Complexity: O(n)
+// Merge Sort is the first non-O(1) space sorting algorithm we've seen thus far.
+
+// The larger the size of our input array, the greater the number of subarrays we must create in memory. These are not free! They each take up finite space, and we will need a new subarray for each element in the original input. Therefore, Merge Sort has a linear space complexity, O(n).
+
+// When should we use Merge Sort?
+// Unless we, the engineers, have access in advance to some unique, exploitable insight about our dataset, it turns out that O(n log n) time is the best we can do when sorting unknown datasets.
+
+// That means that Merge Sort is fast! It's way faster than Bubble Sort, Selection Sort, and Insertion Sort. However, due to its linear space complexity, we must always weigh the tradeoff between speed and memory consumption when making the choice to use Merge Sort. Consider the following:
+
+// If you have unlimited memory available, use it, it's fast!
+// If you have a decent amount of memory available and a medium sized dataset, run some tests first, but use it!
+// If you have very limited memory and you've got time to kill, maybe you should consider other options.
+// If you have very limited memory and no time to kill...well, you're going to have to do some data analysis to look for some exploitable feature of the data set, but that takes human time.
--- radix_sort_project/lib/radix_sort.js
+++ radix_sort_project/lib/radix_sort.js
@@ -34,3 +34,30 @@ function getMaxDigits(nums){
 module.exports = {
     radixSort
 };
+
+
+// https://open.appacademy.io/learn/swe-in-person/career-quest/radix-sort-notes
+// Time and Space Complexity Analysis
+// Time Complexity
+// In general, the best, average, and worst case time complexities of Radix Sort are all the same.
+
+// Since this algorithm requires iterating over all n elements of the input array, and doing so k times, where k is the length (number of digits) of the longest integer in the array, we wind up with a run time of O(n * k). This makes Radix Sort faster than any of the previous comparison-based algorithms we've seen earlier in the course!
+
+// However, there is actually some debate in the computer science community over this topic. There exists a camp that believes that when Radix Sort's input array contains entirely unique values, due to a characteristic of the way computers store numerical data, that k becomes log k. (See Wikipedia: Radix Sort)
+
+// If this is true, the absolute worst case scenario becomes the case where the length of the longest integer in the input array, k, is equal to (or greater than) the total number of elements in the array, n. In this scenario, we wind up with an O(n log(k)), or approximately O(n log(n)), run time, making Radix Sort, at worst, equal in speed to our fastest comparison-based sorting algorithm.
+
+// Though it will require some additional research, this may be a worthwhile talking point in an interview setting!
+
+// Space Complexity
+// Radix Sort is an O(n + k) space algorithm.
+
+// The amount of memory consumed by the algorithm increases relative to both the size of the input array and the length of the longest integer.
+
+// When should we use Radix Sort?
+// You should consider using Radix sort whenever you need to:
+
+// Sort a list of any sort of binary data, including numeric, text, or image data in binary format.
+// Sort a list of integers, and you don't know the value of the largest element in the list.
+// If you do know the largest element in the list, see countingSort!
+// Radix Sort's run time, O(n * k), is depedendent on the length (number of digits) of the largest integer in the input, k. For this reason, it is fastest when k is relatively small.
