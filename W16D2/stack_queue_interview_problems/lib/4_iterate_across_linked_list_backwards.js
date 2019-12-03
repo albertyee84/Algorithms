9fde87cf37795848b700c8ff89a0ffff2de2e2d6
Albert Yee <albertyee84@gmail.com>
2019-12-03 11:21:09 -0800

    add solution utilizing stack

--- W16D2/stack_queue_interview_problems/lib/4_iterate_across_linked_list_backwards.js
+++ W16D2/stack_queue_interview_problems/lib/4_iterate_across_linked_list_backwards.js
@@ -28,16 +28,70 @@
 
 function iterateAcrossLinkedListBackwards(linkedList) {
     // TODO: Implement the iterateAcrossLinkedListBackwards function here
-    if(linkedList.length === 0) return "";
-    let listlength = linkedList.length;
-    let queue = [];
-    for (let i = listlength-1; i >= 0; i--){
-        queue.push(String(linkedList.get(i).value));
-        linkedList.get(i).next = null;
-    }
-    if(queue.length === 1) return queue[0];
-    return queue.join(" -> ");
+    const stack = new Stack();
+    let current = linkedList.head;
+    let result = '';
 
+    while (current != null) {
+        stack.push(current);
+        current = current.next;
+    }
+
+    while (stack.size() > 0) {
+        if(stack.size() > 1) {
+            result += `${stack.pop().value} -> `;
+        } else {
+            result += `${stack.pop().value}`;
+        }
+    }
+
+    return result;
 }
 
 exports.iterateAcrossLinkedListBackwards = iterateAcrossLinkedListBackwards;
+
+class Node {
+    constructor(val) {
+        this.value = val;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
+
+    push(val) {
+        let newNode = new Node(val.value);
+        if (this.length === 0) {
+            this.top = newNode;
+            this.bottom = newNode;
+        } else {
+            let temp = this.top;
+            this.top = newNode;
+            this.top.next = temp;
+        }
+        this.length++;
+        return this.size();
+    }
+
+    pop() {
+        let temp = this.top;
+        if (this.length === 0) return null;
+        if (this.length === 1) {
+            this.top = null;
+            this.bottom = null;
+        } else {
+            this.top = this.top.next;
+        }
+        this.length--;
+        return temp;
+    }
+
+    size() {
+        return this.length;
+    }
+}
